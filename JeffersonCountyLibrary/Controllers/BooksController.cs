c29886da2c4db2651901eefa16382870d2cfae0e
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-10-18 09:47:27 -0600

    Add project files.

--- JeffersonCountyLibrary/Controllers/BooksController.cs
+++ JeffersonCountyLibrary/Controllers/BooksController.cs
@@ -45,7 +45,6 @@ namespace JeffersonCountyLibrary.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public IActionResult CheckOut(int? id)
         {
             var book = _context.Books.Find(id);
